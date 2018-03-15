16d53349f347fbe4b572a89767e2f53c9477883a
Jim Schaub <jschau@me.com>
2018-03-15 13:47:36 -0500

    Expanded print statement text.

--- GitExampleXC9/ViewController.swift
+++ GitExampleXC9/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("Some change")
+        print("Some changes made in Jim's branch.")
     }
 
     override var representedObject: Any? {
